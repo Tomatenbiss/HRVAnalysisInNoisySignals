dbbbd4f039cdf81eb27a4116a1db96c7d70c5193
tb <tb@tbs-MacBook-Pro.fritz.box>
2017-05-28 09:45:16 +0100

    Improved iterator interval for RR Tachogram creation.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr
 from astropy.stats import LombScargle
 
+#Getting the data
 f = open("sampleDataEcgEda.txt", "r")
 dataEcg = []
 
@@ -11,30 +12,40 @@ for line in f:
 	lineValues = line.split(",")
 	dataEcg.append(float(lineValues[1]))
 
+
+
+#Perform QRS detection
 ecgOut = ecg.ecg(signal=dataEcg, sampling_rate=1000., show=False)
+
+#Calculate RR Tachogram
 rPeaks = ecgOut[2]
 rrTachogram = []
 prevPeak = rPeaks[0]
-for peak in rPeaks[2:-1]:
+for peak in rPeaks[1:(len(rPeaks))]:
 	rrTachogram.append(peak - prevPeak)
 	prevPeak = peak
 
 
+#Calculate median heartbeat template
 templatesForCorrCoef = ecgOut[4]
 templates = templatesForCorrCoef[-10:-1]
 medianTemplate = [x / len(templates) for x in [sum(x) for x in zip(*templates)]]
 
+#Calculate correlation coeffcients
 corrCoeffs = []
 for template in templatesForCorrCoef:
 	corrCoeffs.append(pearsonr(template, medianTemplate)[0])
 
+#create RR Tachogram without bad quality heart beats
 rrTachogramAfterSqi = []
 t = []
-cnt = 1
+tPeaks = []
+cnt = 0
 for peak in rrTachogram:
 	if corrCoeffs[cnt] >= 0.9:
 		rrTachogramAfterSqi.append(peak)
 		t.append(cnt)
+		tPeaks.append(rPeaks[cnt])
 	cnt = cnt + 1;
 
 #rrTachogramAfterSqi = []
@@ -48,12 +59,12 @@ for peak in rrTachogram:
 
 
 
-frequency, power = LombScargle(t, rrTachogramAfterSqi).autopower()
-power = np.convolve(power, np.ones(20)/20)
-power = power[0:(len(frequency))]
-plt.plot(frequency, power)
-plt.xlim(0, 0.4)
-plt.ylim(0, 0.01)
-plt.show()
-#plt.plot(rrTachogramAfterSqi)
+#frequency, power = LombScargle(tPeaks, rrTachogramAfterSqi).autopower()
+#power = np.convolve(power, np.ones(20)/20)
+#power = power[0:(len(frequency))]
+#plt.plot(frequency, power)
+#plt.xlim(0, 0.4)
+#plt.ylim(0, 0.01)
 #plt.show()
+plt.plot(tPeaks, rrTachogramAfterSqi)
+plt.show()
